0503dead7810bd93722de80a79bbdcab480ae29f
Michael Padin <padinmichael201@gmail.com>
2024-12-05 14:00:37 +0800

    fix: resolve lag animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,25 +8,27 @@ export default function Home() {
 		<main className="relative z-10">
 			<div className="mt-4 space-y-8 p-4">
 				<div className="space-y-2">
-					<h1 className="text-white font-black text-center text-4xl uppercase">
-						Team Padin
-					</h1>
-					<div className="flex justify-center">
-						<MemberList />
-					</div>
+					<MotionDiv initial={{ scale: 0 }} animate={{ scale: 1 }} >
+						<h1 className="text-white font-black text-center text-4xl uppercase">
+							Team Padin
+						</h1>
+						<div className="flex justify-center">
+							<MemberList />
+						</div>
+					</MotionDiv>
 				</div>
 				<div className="grid lg:grid-cols-2 max-w-screen-lg mx-auto gap-4">
 					<MotionDiv
 						initial={{ opacity: 0, x: -100 }}
 						animate={{ opacity: 1, x: 0 }}
-						transition={{ duration: 0.5 }}
+						transition={{ duration: 0.35 }}
 					>
 						<SymmetricCipher />
 					</MotionDiv>
 					<MotionDiv
 						initial={{ opacity: 0, x: 100 }}
 						animate={{ opacity: 1, x: 0 }}
-						transition={{ duration: 0.5 }}
+						transition={{ duration: 0.35 }}
 					>
 						<FunFact />
 					</MotionDiv>
